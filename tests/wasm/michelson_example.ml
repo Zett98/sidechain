f0951febd19620fdd4a14b8458b58db6e8b33b68
Zett98 <p.khrystenko@gmail.com>
2022-08-10 21:34:13 +0200

    up

--- tests/wasm/michelson_example.ml
+++ tests/wasm/michelson_example.ml
@@ -1,135 +1,135 @@
-let retard =
-  {|
-{ parameter (or (int %decrement) (int %increment)) ;
-storage int ;
-code { DUP ;
-       CDR ;
-       DIP { DUP } ;
-       SWAP ;
-       CAR ;
-       IF_LEFT
-         { DUP ;
-           DIP { DIP { DUP } ; SWAP } ;
-           PAIR ;
-           DUP ;
-           CDR ;
-           DIP { DUP ; CAR } ;
-           SUB ;
-           DIP { DROP 2 } }
-         { DUP ;
-           DIP { DIP { DUP } ; SWAP } ;
-           PAIR ;
-           DUP ;
-           CDR ;
-           DIP { DUP ; CAR } ;
-           ADD ;
-           DIP { DROP 2 } } ;
-       NIL operation ;
-       PAIR ;
-       DIP { DROP 2 } } }
-       |}
-
 open Wasm
-module VarMap = Map.Make (String)
-
-type space = {
-  mutable map : int32 VarMap.t;
-  mutable count : int32;
-}
-
-let empty () = { map = VarMap.empty; count = 0l }
-
-type types = {
-  space : space;
-  mutable list : Ast.type_ list;
-}
-
-let empty_types () = { space = empty (); list = [] }
-
-type context = {
-  types : types;
-  tables : space;
-  memories : space;
-  funcs : space;
-  locals : space;
-  globals : space;
-  datas : space;
-  elems : space;
-  labels : int32 VarMap.t;
-  deferred_locals : (unit -> unit) list ref;
-}
-
-let empty_context () =
-  {
-    types = empty_types ();
-    tables = empty ();
-    memories = empty ();
-    funcs = empty ();
-    locals = empty ();
-    globals = empty ();
-    datas = empty ();
-    elems = empty ();
-    labels = VarMap.empty;
-    deferred_locals = ref [];
-  }
-
-module EmptyMod = struct
-  (* creates an emptyu module  *)
-  (* let empty_module =
-     {
-       types = [];
-       globals = [];
-       tables = [];
-       memories = [];
-       funcs = [];
-       start = None;
-       elems = [];
-       datas = [];
-       imports = [];
-       exports = [];
-     }
-  *)
-  let create_empty () = Wasm.Ast.empty_module
-end
 
 let body fmtr =
-  (* let main_func =
-       {|
-     (func (export "main") (param $tup i32) (result i32)
-       call $nil
-       local.get $tup
-       call $unpair
-       call $z_add
-       local.tee $tup
-       call $pair
-     )
-     |}
-     in *)
   Fmt.(
     str
       {|
 (module 
   (import "env" "unpair" (func $unpair (param i32) (result i32 i32)))
   (import "env" "pair" (func $pair (param i32 i32) (result i32)))
+  (import "env" "car" (func $car (param i32) (result i32)))
+  (import "env" "cdr" (func $cdr (param i32) (result i32)))
   (import "env" "z_add" (func $z_add (param i32 i32) (result i32)))
+  (import "env" "z_sub" (func $z_sub (param i32 i32) (result i32)))
   (import "env" "nil" (func $nil (result i32)))
   (memory (export "memory") 1)
+  (func $swap (param i32) (param i32) (result i32 i32) 
+    local.get 1
+    local.get 0
+  )
+  (func $dup (param i32) (result i32 i32) 
+    local.get 0 
+    local.get 0
+  )
   %s
 )
 |}
       fmtr)
 
+let retard =
+  {|
+    { parameter (or (or (int %decrement) (int %increment)) (unit %reset)) ;
+    storage int ;
+    code { UNPAIR ;
+           IF_LEFT { IF_LEFT { SWAP ; SUB } { ADD } } { DROP 2 ; PUSH int 0 } ;
+           NIL operation ;
+           PAIR } }  
+  |}
+
 module Mapper = struct
   open Tezos.Michelson.Michelson_v1_primitives
 
-  let mapper (x : prim) =
-    (match x with
-    | I_UNPAIR -> "call $unpair"
-    | I_ADD -> "call $z_add"
-    | I_NIL -> "call $nil"
-    | I_PAIR -> "call $pair"
-    | _ -> assert false)
-    |> fun x -> x
+  let ppt fmt =
+    Format.pp_print_list
+      ~pp_sep:(fun fmt () -> Format.fprintf fmt "\n")
+      Format.pp_print_string fmt
+
+  let rec mapper (x : prim) args =
+    let open Tezos_micheline.Micheline in
+    match (x, args) with
+    | I_DIP, [Seq (_, x)] ->
+      Format.asprintf
+        {|
+        block (param i32 i32) (result i32 i32)
+          call $swap
+          %a
+        end
+        call $swap
+    |}
+        ppt
+        (List.map
+           (fun x ->
+             let open Tezos_micheline.Micheline in
+             match x with
+             | Prim (_, prim, args, _) -> mapper prim args
+             (* | Int (_, x) ->
+                let open Wasm.Script in
+                let open Source in
+                Ast.Val (Values.Ref (ExternRef x @@ no_region) @@ no_region) *)
+             | _ -> assert false)
+           x)
+    | I_UNPAIR, _ -> "call $unpair"
+    | I_DROP, [Int (_, num)] ->
+      Format.asprintf "%a" ppt (List.init (Z.to_int num) (fun _ -> "drop\n"))
+    | I_ADD, _ -> "call $z_add"
+    | I_SUB, _ -> "call $z_sub"
+    | I_NIL, _ -> "call $nil"
+    | I_PAIR, _ -> "call $pair"
+    | I_DUP, _ -> {|
+      call $dup
+      |}
+    | I_SWAP, _ -> {|call $swap|}
+    | I_CDR, _ -> {|
+     call $dup
+     call $cdr 
+     |}
+    | I_CAR, _ -> {|
+    call $dup
+    call $car 
+    |}
+    | I_PUSH, [_; Int (_, x)] -> Format.sprintf "i32.const %d" (Z.to_int x)
+    | I_IF_LEFT, [Seq (_, x); Seq (_, y)] ->
+      Format.asprintf
+        {|
+      call $dup
+      call $car
+      i32.eqz 
+      (if (param i32 i32) (result i32) 
+        (then
+          call $cdr
+          %a) 
+        (else
+          call $cdr
+          %a) 
+      )
+      |}
+        ppt
+        (List.map
+           (fun x ->
+             let open Tezos_micheline.Micheline in
+             match x with
+             | Prim (_, prim, args, _) -> mapper prim args
+             (* | Int (_, x) ->
+                let open Wasm.Script in
+                let open Source in
+                Ast.Val (Values.Ref (ExternRef x @@ no_region) @@ no_region) *)
+             | _ -> assert false)
+           y)
+        ppt
+        (List.map
+           (fun x ->
+             let open Tezos_micheline.Micheline in
+             match x with
+             | Prim (_, prim, args, _) -> mapper prim args
+             (* | Int (_, x) ->
+                let open Wasm.Script in
+                let open Source in
+                Ast.Val (Values.Ref (ExternRef x @@ no_region) @@ no_region) *)
+             | _ -> assert false)
+           x)
+    | x, _ ->
+      print_endline @@ Tezos.Michelson.Michelson_v1_primitives.string_of_prim x;
+      assert false
 end
 
 let contract_addition =
@@ -146,11 +146,12 @@ let contract_addition =
          ([]),(action + store)
   *)
   {|
-  
-  { parameter int ;
-  storage int ;
-  code { UNPAIR ; ADD ; NIL operation ; PAIR } }
 
+  { 
+   parameter int ;
+  storage int ;
+  code { UNPAIR ; DIP { DIP {DUP; DUP; ADD}; DROP }; SUB ; NIL operation ; PAIR } 
+  }
   |}
 
 module Externs = struct
@@ -213,6 +214,7 @@ end
 type values =
   | Pair : int32 * int32 -> values
   | List : int32 list -> values
+  | Bool : int32 -> values
   | Num  : Z.t -> values
 
 let rec pp_values table fmt = function
@@ -224,17 +226,27 @@ let rec pp_values table fmt = function
     let lst = List.map (fun x -> M.find x table) x in
     Format.fprintf fmt "List [%a]" (Fmt.list pp) lst
   | Num z -> Format.fprintf fmt "Num %s" (Z.to_string z)
+  | Bool x ->
+    Format.fprintf fmt "Bool %b" (if Int32.equal x 0l then false else true)
 
 let incrr r = r := Int32.succ !r
 
 let make ~gas ~module_ ~custom =
   let empty = ref M.empty in
   let counter = ref 0l in
-  empty := M.add !counter (Num (Z.of_int 5)) !empty;
+  empty := M.add !counter (Bool 0l) !empty;
   incrr counter;
-  empty := M.add !counter (Num (Z.of_int 5)) !empty;
+  empty := M.add !counter (Bool 1l) !empty;
   incrr counter;
-  let empty = ref @@ M.add !counter (Pair (0l, 1l)) !empty in
+  empty := M.add !counter (Num (Z.of_int 10)) !empty;
+  incrr counter;
+  empty := M.add !counter (Num (Z.of_int 2)) !empty;
+  incrr counter;
+  empty := M.add !counter (Pair (1l, 3l)) !empty;
+  incrr counter;
+  empty := M.add !counter (Pair (6l, 2l)) !empty;
+  incrr counter;
+  empty := M.add !counter (Pair (1l, 4l)) !empty;
   incrr counter;
   let open Core in
   let _custom =
@@ -249,6 +261,16 @@ let make ~gas ~module_ ~custom =
       incrr counter;
       Some [Values.Num (I32 ret)] in
     Externs.[func ([I32; I32], Some [I32]) adder] in
+  let[@warning "-8"] z_sub =
+    let adder _ [Values.Num (I32 two); Values.Num (I32 one)] =
+      Format.printf "%ld %ld" one two;
+      let[@warning "-8"] (Num ptr1) = M.find one !empty in
+      let[@warning "-8"] (Num ptr2) = M.find two !empty in
+      empty := M.add !counter (Num (Z.sub ptr1 ptr2)) !empty;
+      let ret = !counter in
+      incrr counter;
+      Some [Values.Num (I32 ret)] in
+    Externs.[func ([I32; I32], Some [I32]) adder] in
   let[@warning "-8"] pair =
     let adder _ [Values.Num (I32 one); Values.Num (I32 two)] =
       empty := M.add !counter (Pair (one, two)) !empty;
@@ -256,6 +278,16 @@ let make ~gas ~module_ ~custom =
       incrr counter;
       Some [Values.Num (I32 ret)] in
     Externs.[func ([I32; I32], Some [I32]) adder] in
+  let[@warning "-8"] car =
+    let adder _ [Values.Num (I32 one)] =
+      let[@warning "-8"] (Pair (ret, _)) = M.find one !empty in
+      Some [Values.Num (I32 ret)] in
+    Externs.[func ([I32], Some [I32]) adder] in
+  let[@warning "-8"] cdr =
+    let adder _ [Values.Num (I32 one)] =
+      let[@warning "-8"] (Pair (_, ret)) = M.find one !empty in
+      Some [Values.Num (I32 ret)] in
+    Externs.[func ([I32], Some [I32]) adder] in
   let[@warning "-8"] nil =
     let adder _ [] =
       empty := M.add !counter (List []) !empty;
@@ -266,7 +298,7 @@ let make ~gas ~module_ ~custom =
   let[@warning "-8"] unpair =
     let adder _ [Values.Num (I32 one)] =
       let[@warning "-8"] (Pair (one, two)) = M.find one !empty in
-      Some [Values.Num (I32 one); Values.Num (I32 two)] in
+      Some [Values.Num (I32 two); Values.Num (I32 one)] in
     Externs.[func ([I32], Some [I32; I32]) adder] in
   let uninit : Wasm.Instance.module_inst Set_once.t = Set_once.create () in
   let imports =
@@ -275,7 +307,7 @@ let make ~gas ~module_ ~custom =
         (Externs.to_wasm (fun () ->
              let instance = Set_once.get_exn uninit Lexing.dummy_pos in
              get_memory instance))
-      (unpair @ pair @ z_add @ nil) in
+      (unpair @ pair @ car @ cdr @ z_add @ z_sub @ nil) in
   let instance = Eval.init gas module_ imports in
   (* XXX: Is this the better way of doing this? *)
   Set_once.set_exn uninit Lexing.dummy_pos instance;
@@ -305,7 +337,7 @@ let run fmt =
       | ExternFunc x -> x
       | _ -> assert false in
     let[@warning "-8"] [Values.Num (I32 x)] =
-      Wasm.Eval.invoke (ref max_int) func_inst [Values.Num (I32 2l)] in
+      Wasm.Eval.invoke (ref max_int) func_inst [Values.Num (I32 5l)] in
     let _result = M.find x !table in
     let pp = pp_values !table in
     Format.printf "result is %a\n" pp _result;
@@ -315,7 +347,7 @@ let run fmt =
 let () =
   let open Tezos_micheline in
   let ff =
-    let r, _ = Micheline_parser.tokenize contract_addition in
+    let r, _ = Micheline_parser.tokenize retard in
     let r, _ = Micheline_parser.parse_expression r in
     let r = Micheline.strip_locations r |> Micheline.root in
     r in
@@ -337,7 +369,7 @@ let () =
           (fun x ->
             let open Micheline in
             match x with
-            | Prim (_, prim, _, _) -> Mapper.mapper prim
+            | Prim (_, prim, args, _) -> Mapper.mapper prim args
             (* | Int (_, x) ->
                let open Wasm.Script in
                let open Source in
@@ -354,5 +386,6 @@ let () =
            ~pp_sep:(fun fmt () -> Format.fprintf fmt "\n")
            Fmt.string)
         (["local.get 0"] @ result)
+    | Prim _ -> assert false
     | _ -> assert false in
   run ff
